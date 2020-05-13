7910a9fc1510271fd62770e1387cfe4bcda53a54
Exo-Byte <vijaysh312@gmail.com>
2020-05-13 13:04:14 -0700

    Initial commit

---
+++ install.lua
@@ -0,0 +1,40 @@
+-- OptimisticSide
+
+local HttpService = game:GetService("HttpService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ServerStorage = game:GetService("ServerStorage")
+
+
+function Install(HttpEnabled)
+	if not HttpEnabled then
+		warn("RoService - HTTP is not enabled; Enabling...")
+		HttpService.HttpEnabled = true
+	end
+	
+	local Success, Fail = pcall(function()
+		local ServicesModule = Instance.new("ModuleScript", ReplicatedStorage)
+		ServicesModule.Name = "Services"
+		
+		local Success, Result = pcall(function()
+			return HttpService:GetAsync("https://raw.githubusercontent.com/Accutrix/RoService/src/Services.lua")
+		end)
+		
+		if Success then
+			ServicesModule.Source = Result
+		else
+			warn("RoService - Unable to get source of Services module")
+		end
+		
+		local SharedServices = Instance.new("Folder", ReplicatedStorage)
+		local ServerServices = Instance.new("Folder", ServerStorage)
+		
+		SharedServices.Name = "SharedServices"
+		ServerServices.Name = "ServerServices"
+	end)
+	
+	if Success then
+		warn("RoService - Installation successful")
+	else
+		warn("RoService - Could not install properly | " .. Fail)
+	end
+end
---
+++ src/Services.lua
@@ -0,0 +1,71 @@
+-- OptimisticSide
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ServerStorage = game:GetService("ServerStorage")
+local RunService = game:GetService("RunService")
+
+local SharedServices = ReplicatedStorage:WaitForChild("SharedServices")
+
+local Services = { }
+
+
+function Services:FindService(ServiceFolder, ServiceName)
+	for _, SubFolder in pairs(ServiceFolder:GetChildren()) do
+		for _, Service in pairs(SubFolder:GetChildren()) do
+			if ServiceName == Service.Name or ServiceName == Service.Name .. "Service" then
+				return Service
+			end
+		end
+	end
+end
+
+
+function Services:GetService(ServiceName, ShouldntRequire)
+	local Service
+	if RunService:IsServer() then
+		local FromServer = Services:FindService(ServerStorage:WaitForChild("ServerServices"), ServiceName)
+		if FromServer then
+			Service = FromServer
+		end
+	end
+	if not Service then
+		local FromShared = Services:FindService(SharedServices, ServiceName)
+		if FromShared then
+			Service = FromShared
+		end
+	end
+	if Service then
+		if not ShouldntRequire then
+			local Success, Result = pcall(require, Service)
+			repeat wait() until Success or Result
+			if Success then
+				Service = Result
+			else
+				warn("Services - Unable to load service '" .. Service.Name .. "' | " .. Result)
+			end
+		end
+	else
+		local Success, GameService = pcall(function()
+			return game:GetService(ServiceName)
+		end)
+		if Success then
+			Service = GameService
+		end
+	end
+	return Service
+end
+
+
+return setmetatable({ }, {
+	__index = function(self, Index)
+		local FromServices = rawget(Services, Index)
+		if FromServices then
+			return FromServices
+		else
+			local Service = Services:GetService(Index)
+			if Service then
+				return Service
+			end
+		end
+	end;
+})
